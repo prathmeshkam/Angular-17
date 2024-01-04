4998497c2eac5498f025674338b7fc873ff2a1e9
Prathmesh Kamble <prathmesh2001kamble@gmail.com>
2024-01-04 21:09:52 +0530

    Angular Basic

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  template: "<h1 class='p'>Hello this is Angular 17</h1>",
+  styles: ".p{color:red;background-color:yellow}"
+})
+export class LoginComponent {
+
+}
