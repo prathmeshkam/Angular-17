30080e5bb77a97fafc5b928c590738863bde2153
Prathmesh Kamble <prathmesh2001kamble@gmail.com>
2024-01-05 18:38:01 +0530

    Created header component and added it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,LoginComponent],
+  imports: [CommonModule, RouterOutlet,LoginComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
