932659cd77a6887e70bc3e52e29bea91b11af6f8
Prathmesh Kamble <prathmesh2001kamble@gmail.com>
2024-01-12 16:30:40 +0530

    Basic Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Child1Component } from './child1/child1.component';
+import { ChildComponent } from './child/child.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'child' , component:ChildComponent},
+    {path:'child1' , component:Child1Component}
+];
