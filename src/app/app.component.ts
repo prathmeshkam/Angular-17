1f2e0a7d478bc341a4ac624cb378b00d1e05924f
Prathmesh Kamble <prathmesh2001kamble@gmail.com>
2024-01-16 22:51:13 +0530

    Module Routing in 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { RouterLink } from '@angular/router';
+import { AdminModule } from './admin/admin.module';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,RouterLink,AdminModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
